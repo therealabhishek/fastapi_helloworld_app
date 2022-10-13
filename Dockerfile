8bc53bceb263a6585c12254395be25ed013b3e6a
Abhishek Kulkarni <kul.abhishake@gmail.com>
2022-10-13 11:18:02 +0530

    app files created

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8000
+
+CMD ["uvicorn", "app:app", "--host=0.0.0.0", "--reload"]
+
+
---
+++ app.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return "Hello Boys!"
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=8000, reload = True)
